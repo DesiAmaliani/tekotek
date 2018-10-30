4c5b4bb029cda51a09f742a713990371c786b2d7
Desi_Amaliani <desi.amaliani@gmail.com>
2018-10-31 03:15:57 +0700

    php tekotek

--- tekotek.php
+++ tekotek.php
@@ -7,11 +7,11 @@
 <?php
 	for($f=100;$f>0;$f--){
 		if($f==100){
-			echo '<p style="font-family:"Helvetica";">'."Tekotek-kotek anak ayam turun $f".'<br></p>';
+			echo '<p style="font-family:Helvetica;">'."Tekotek-kotek anak ayam turun $f".'<br></p>';
 		}elseif(($f%2)==1){
-			echo '<p style="color: orange; font-family: "Helvetica";">'."Mati Satu tinggal $f".'<br></p>';
+			echo '<p style="color: orange; font-family: Helvetica;">'."Mati Satu tinggal $f".'<br></p>';
 		}else{
-			echo '<p style="color: brown; font-family: "Helvetica";">'."Mati Satu tinggal $f".'<br></p>';
+			echo '<p style="color: brown; font-family: Helvetica;">'."Mati Satu tinggal $f".'<br></p>';
 		}
 	}
 ?>
